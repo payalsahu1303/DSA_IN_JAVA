b43fc3d64aa045c21163e091010fffa6f78b28d2
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2025-06-22 08:47:24 +0530

    Add files via upload

---
+++ DYNAMIC PROGRAMMING/PerfectSubsetSum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PerfectSubsetSum {
+    static int perfectSum(int[] nums, int target) {
+        int n = nums.length;
+        int[][] t = new int[n + 1][target + 1];
+
+        for (int i = 0; i <= n; i++) {
+            t[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+                if (nums[i - 1] <= j) {
+                    t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
+                } else {
+                    t[i][j] = t[i - 1][j];
+                }
+            }
+        }
+        return t[n][target];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.println(perfectSum(nums, target));
+    }
+}
