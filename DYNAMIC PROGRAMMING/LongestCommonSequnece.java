7f8c9652ab286b029a2385d327a702fd122761c5
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2025-06-28 08:05:41 +0530

    Create LongestCommonSequnece.java

---
+++ DYNAMIC PROGRAMMING/LongestCommonSequnece.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class LongestCommonSequence {
+    static String lcs(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] t = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    t[i][j] = 1 + t[i - 1][j - 1];
+                } else {
+                    t[i][j] = Math.max(t[i][j - 1], t[i - 1][j]);
+                }
+            }
+        }
+
+        int i = m, j = n;
+        StringBuilder sb = new StringBuilder();
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                sb.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (t[i][j - 1] > t[i - 1][j]) {
+                j--;
+            } else {
+                i--;
+            }
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        System.out.println(lcs(s1, s2));
+    }
+}
